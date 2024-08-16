cea1be3e31d8158c058758fc9ec1943314874eab
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-08-16 15:51:02 -0400

    initial player
    platform, player, player script, and camera follow

---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
