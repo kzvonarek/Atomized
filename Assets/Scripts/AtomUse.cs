81e8683ca77752e269e4d4811dd4c51ac1eb0537
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-08-18 02:50:04 -0400

    helium use functionality
    helium can be consumed with UI icon, just need to add icon disappearing

--- Assets/Scripts/AtomUse.cs
+++ Assets/Scripts/AtomUse.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AtomUse : MonoBehaviour
 {
     // helium functionality
     private FuelManager fuelBarScript;
+    [SerializeField] GameObject player;
+    private List<GameObject> playerAtomList;
 
     void Start()
     {
         // helium functionality
         fuelBarScript = GameObject.FindWithTag("Fuel Manager").GetComponent<FuelManager>();
+
+        // get atom List (currAtoms) from player
+        playerAtomList = player.GetComponent<PlayerBonding>().currAtoms;
     }
 
     // destroy helium atom, and increase fuel by set amount
@@ -18,5 +24,16 @@ public class AtomUse : MonoBehaviour
     {
         // add fuel to fuel bar
         fuelBarScript.HeliumCollected(1f);
+
+        // find a helium atom in List/collected atoms and destroy it/remove from List (currAtoms)
+        GameObject heliumAtom = playerAtomList.Find(atom => atom.name == "Helium Atom");
+
+        if (heliumAtom != null)
+        {
+            playerAtomList.Remove(heliumAtom);
+            Destroy(heliumAtom);
+        }
+
+        // hide helium icon (ADD THIS)
     }
 }
