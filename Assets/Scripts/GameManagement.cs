a5c454c791bdf7c42f4927a244a899a9e077bada
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-08-19 01:02:53 -0400

    added timer

--- Assets/Scripts/GameManagement.cs
+++ Assets/Scripts/GameManagement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI timerText;
+    private float timeElapsed;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // timer that showcases hours, minutes, seconds, and milliseconds alive
+        timeElapsed += Time.deltaTime;
+        int hours = Mathf.FloorToInt(timeElapsed / 3600);
+        int minutes = Mathf.FloorToInt(timeElapsed % 3600 / 60);
+        int seconds = Mathf.FloorToInt(timeElapsed % 60);
+        int milliseconds = Mathf.FloorToInt(timeElapsed * 100 % 100);
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", hours, minutes, seconds, milliseconds);
+
+        // score?
     }
 }
