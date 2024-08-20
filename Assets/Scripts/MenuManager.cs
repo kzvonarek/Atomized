53670646e82e8db2bf840e1b56ee475cdbc049ce
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-08-20 05:50:10 -0400

    fixed issue where time would freeze on main menu after death

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
     // main menus
     public void playGame()
     {
