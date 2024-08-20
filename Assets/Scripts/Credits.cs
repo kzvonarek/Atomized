0e29112c70f4af6fcfc9f42b6ea6c675cda7ac68
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-08-20 11:00:38 -0400

    credits

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    /*
+    Credits
+
+    Any art/audio/font/code not made by me are listed below.
+
+    Background
+    https://slashdashgamesstudio.itch.io/shump-free-assets
+
+    Font
+    https://www.dafont.com/rainyhearts.font
+
+    Rotation (code)
+    https://www.youtube.com/watch?v=RNPetUa8_PQ
+
+    BGM Track (Menu)
+    "Mesmerizing Galaxy " Kevin MacLeod (incompetech.com)Licensed under Creative Commons: By Attribution 4.0 License http://creativecommons.org/licenses/by/4.0/
+
+    BGM Track (Gameplay)
+    "Phantom from Space" Kevin MacLeod (incompetech.com)Licensed under Creative Commons: By Attribution 4.0 License http://creativecommons.org/licenses/by/4.0/
+
+    BGM Track (Controls)
+    "Space Jazz" Kevin MacLeod (incompetech.com)Licensed under Creative Commons: By Attribution 4.0 License http://creativecommons.org/licenses/by/4.0/
+
+    SFX
+    https://opengameart.org/content/menu-selection-click (menu buttons)
+    https://opengameart.org/content/alien-blaster (atom pickup)
+    https://opengameart.org/content/pop (helium use)
+    https://opengameart.org/content/skippy-fish-water-sound-collection (h2o use)
+    https://opengameart.org/content/synthesized-explosion (ch4 use)
+    https://opengameart.org/content/insect-or-alien-scream-short (o2 use)
+    https://opengameart.org/content/8-bit-powerup-1 (n2o use)
+    https://opengameart.org/content/mutant-death (game over)
+    */
+}
